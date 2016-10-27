ca68ea673990fdfe3ca60c45d637334896cd6d17
harishpuvvada <hp1047@nyu.edu>
2016-10-27 19:35:58 -0400

    fix thursay

--- retirechat/api/update_percentage.php
+++ retirechat/api/update_percentage.php
@@ -7,9 +7,9 @@ $year = $_SESSION["year"];
 $month = $_SESSION["month"];
 $fundpct = array();
 
-$query = "SELECT fund1pct, fund2pct, fund3pct, fund4pct, fund5pct, fund6pct, fund7pct, fund8pct, fund9pct, fund10pct, totalvalue FROM activity ORDER BY modified DESC LIMIT 0,1";
+$query = "SELECT fund1pct, fund2pct, fund3pct, fund4pct, fund5pct, fund6pct, fund7pct, fund8pct, fund9pct, fund10pct FROM activity ORDER BY modified DESC LIMIT 0,1";
 
-if ($result = mysqli_query($connection, $query)) {
+if ($result = mysqli_query($conn, $query)) {
   while ($row = mysqli_fetch_row($result)) {
    /* $total++;
     array_push($name, $row[0]);
