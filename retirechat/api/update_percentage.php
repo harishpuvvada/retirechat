b6568ab202b0c4c3864deb4216c20014f265ccee
harishpuvvada <hp1047@nyu.edu>
2016-10-27 18:39:51 -0400

    fix thursay

--- retirechat/api/update_percentage.php
+++ retirechat/api/update_percentage.php
@@ -15,26 +15,20 @@ $name=array();
 $thisYear = array();
 $total = 0; // number of rows
 
-if ($result = mysqli_query($conn, $query)) {
+if ($result = mysqli_query($connection, $query)) {
   while ($row = mysqli_fetch_row($result)) {
-    $total++;
+   /* $total++;
     array_push($name, $row[0]);
-    array_push($thisYear, $row[1]);
+    array_push($thisYear, $row[1]);*/
 
-//$ result = [{
-  //"name" : "invest grade bond fund a",
-  //"portfolio" : 0,
-  //"percentage" : ""
-//},{
-  //"name" : "invest grade bond fund a",
-  //"portfolio" : 0,
-  //"percentage" : ""
-//},{
-  //"name" : "invest grade bond fund a",
-  //"portfolio" : 0,
-  //"percentage" : ""
-//}]
+var_dump($row);
+echo"<h r/>";
+
 }
+}
+else{
+
+echo "something wrong";
 }
 
 echo $result;
@@ -42,3 +36,22 @@ echo $result;
 
 
 ?>
+
+
+
+
+
+
+<!-- //$ result = [{
+  //"name" : "invest grade bond fund a",
+  //"portfolio" : 0,
+  //"percentage" : ""
+//},{
+  //"name" : "invest grade bond fund a",
+  //"portfolio" : 0,
+  //"percentage" : ""
+//},{
+  //"name" : "invest grade bond fund a",
+  //"portfolio" : 0,
+  //"percentage" : ""
+//}] -->
--- retirechat/js/update_percentage.js
+++ retirechat/js/update_percentage.js
@@ -1,5 +1,5 @@
 //author:Harry
-App.getData  = function() {
+/*App.getData  = function() {
     $.ajax({
     type: "GET",
     url: "api/update_percentages.php",
@@ -9,4 +9,42 @@ App.getData  = function() {
       $('#lastyear-val').text(data[0].last_year);
     }
   });
+};*/
+
+(function(){
+	
+var App = function(){
+
+}
+
+App.bindBtn = function(){
+	var t = this;
+	$('#perc').on("change keyup", function(e){
+		t.getData(e.target.name, e.target.text) 
+	})
+}
+
+App.getData  = function(name, value) {
+	$.ajax({
+    type: "GET",
+    url: "api/update_percentages.php",
+    data: {
+    	"name": name,
+    	"percentage": value
+    },
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
+      // $('#'+name).text(data[0].last_year);
+    }
+  });
 };
+
+App.init = function(){
+	var t = this;
+	t.bindBtn();
+}
+
+App = new App();
+App.init();
+}())
