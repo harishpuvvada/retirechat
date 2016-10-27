16600fdfce8704d96510c93edfe564e0f204096f
harishpuvvada <hp1047@nyu.edu>
2016-10-27 02:17:48 -0400

    fetching data from db

--- retirechat/admin/include.php
+++ retirechat/admin/include.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("127.0.0.1", "root", "harish0625", "db_retirechat");
+$conn = mysqli_connect("localhost", "root", "BAgowan13sql", "retire5");
 
 if (mysqli_connect_errno()) {
   die('Unable to connect to database [' . mysqli_connect_error() . ']');
