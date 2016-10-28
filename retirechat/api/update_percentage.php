43a16fbf5b22046af6ddf1e01c5be17ce1ac0992
harishpuvvada <hp1047@nyu.edu>
2016-10-27 20:50:27 -0400

    fix thursay

--- retirechat/api/update_percentage.php
+++ retirechat/api/update_percentage.php
@@ -7,10 +7,16 @@ $year = $_SESSION["year"];
 $month = $_SESSION["month"];
 $fundpct = array();
 
-$query = "SELECT fund1pct, fund2pct, fund3pct, fund4pct, fund5pct, fund6pct, fund7pct, fund8pct, fund9pct, fund10pct FROM activity ORDER BY modified DESC LIMIT 0,1";
+$query1 = "SELECT fund1pct, fund2pct, fund3pct, fund4pct, fund5pct, fund6pct, fund7pct, fund8pct, fund9pct, fund10pct FROM activity ORDER BY modified DESC LIMIT 0,1";
 
-if ($result = mysqli_query($conn, $query)) {
-  while ($row = mysqli_fetch_assoc($result)) {
+$query2 = "SELECT name FROM fund";
+
+
+
+
+
+if ($result = mysqli_query($conn, $query1)) {
+  while ($row = mysqli_fetch_assoc($result1)) {
    /* $total++;
     array_push($name, $row[0]);
     array_push($thisYear, $row[1]);*/
@@ -28,16 +34,34 @@ echo $row["fund9pct"]."<br />";
 echo $row["fund10pct"]."<br />";
 
 
+}
+}
+else{
+
+echo "something wrong in 1";
+
+}
+
+if ($result = mysqli_query($conn, $query2)) {
+  while ($row = mysqli_fetch_assoc($result2)) {
+  
+
+//var_dump($row);
+var_dump($row);
 
 }
 }
 else{
 
-echo "something wrong";
+echo "something wrong in 2";
 
 }
 
-echo $result;
+
+
+
+
+
 
 
 ?>
